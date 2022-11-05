4e8b1a9685a34e9b63037e903abd28024c9799d9
kamposal <Alexandros.Kampos@arhsdev.onmicrosoft.com>
2022-11-05 11:34:07 +0200

    Creating the repository

---
+++ src/test/java/com/akampos/comments/CommentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akampos.comments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
